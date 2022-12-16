a7735b5cb8cb59ee8241bb3acbdf2797cd11d4f3
Tristan Whitehouse <tsw54@cornell.edu>
2022-12-15 22:56:50 -0500

    commit tree 93dbd0ed192b62c256c3d5fdc7e013f4826f61e1

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solutions in this file
+for (let age = 30; age <40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts)
+
+function writeCards(names, occasion) {
+    const thankYous = new Array(names.length);
+    for (let i = 0; i < names.length; i++) {
+
+    thankYous[i] = `Thank you, ${names[i]}, for the wonderful ${occasion} gift!`;
+    
+    debugger;
+    }
+
+    return thankYous;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+      }
+}
+
+countDown(10)
